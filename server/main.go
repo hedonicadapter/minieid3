38c523b828c7ec5a21038a75a83257b9c3234dda
hedonicadapter <mailservice.samherman@gmail.com>
2025-04-30 15:53:20 +0200

    start

--- server/main.go
+++ server/main.go
@@ -4,6 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"crypto/tls"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http/httptrace"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hedonicadapter/gopher/api/routes"
 	"github.com/hedonicadapter/gopher/config"
@@ -36,5 +43,27 @@ func main() {
 		return ""
 	})
 
+	// ----------------------------------------------------------------------------------
+
+	backendURL := "add new service and put url here"
+	parsedURL, _ := url.Parse(backendURL)
+
+	proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+	proxy.Transport = httptrace.WrapRoundTripper(&http.Transport{
+		MaxIdleConnsPerHost: 20,
+		TLSClientConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		},
+	})
+
+	r.Any("/proxy/*path", func(c *gin.Context) {
+		c.Request.URL.Path = c.Param("path")
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
 	r.Run()
 }
+
+// /*   httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http" */ proxy := httputil.NewSingleHostReverseProxy(&cfg.BackendURL) proxy.Transport = httptrace.WrapRoundTripper(&http.Transport{     MaxIdleConnsPerHost: 20,     TLSClientConfig: &tls.Config{         MinVersion:   tls.VersionTLS12,         Certificates: clientAndCertificates.certificates.Certificates,         RootCAs:      clientAndCertificates.certificates.RootCAs,     }, })
+//
+// proxy.Serve.HTTP(c.Write, c.Request)
